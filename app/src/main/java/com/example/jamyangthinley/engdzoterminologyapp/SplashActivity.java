763697f8de0789e38c613e075e085370415583cb
JamyangThinley <jamyang.thinley97@gmail.com>
2018-10-31 17:01:42 -0700

    first commit

---
+++ app/src/main/java/com/example/jamyangthinley/engdzoterminologyapp/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.jamyangthinley.engdzoterminologyapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int splashTimeOut=2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, splashTimeOut);
+    }
+}
